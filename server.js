745660af06fea3e5803bd7247e2924c37d6ab36d
Gulfarida <gulfaridazhanabergen@gmail.com>
2026-05-30 14:52:36 +0100

    add week2 and week3 code files

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const http = require('http');
-const server = http.createServer((req, res) => {
- res.writeHead(200, { 'Content-Type' : 'text/plain' });
-res.end('Hello World\n');
+const  { SecretManagerServiceClient } =  require ('@google-cloud/secret-manager');
+
+const client = new SecretManagerServiceClient();
+
+const server = http.createServer(async (req, res) => {
+ if (req.url === '/secret' ) { 
+  try{ 
+   const [version] = await client.accessSecretVersion({
+    name: 'projects/strange-aria-402811/secrets/my-api-key/versions/latest',
 });
+  const secret = version.payload.data.toString();
+  res.writeHead(200, { 'Content-Type': 'text/plain'});
+  res.end(`Secret: ${secret}\n` );
+} catch (err) {
+ res.writeHead(500);
+ res.end('Error reading secret\n');
+}
+} else {
+res.writeHead(200, {'Content-Type': 'text/plain' })
+res.end('Hello World\n');
+}
+});
+
 server.listen(8080, () => {
 console.log('server running at http://localhost:8080');
 });
